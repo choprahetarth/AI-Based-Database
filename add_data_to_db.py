7f53323fa695901c72beee2af7aff8bae13ff2c9
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2023-05-31 04:40:02 +0000

    added the true north script

---
+++ add_data_to_db.py
@@ -0,0 +1,19 @@
+import pymongo 
+import pandas as pd
+
+# read the data in pandas
+df = pd.read_csv('data/Twitter_Data.csv').head(2) # reading just 2000 files for the PoC
+df.reset_index(level=0, inplace=True)
+comments = df.to_dict(orient='records')
+
+# # connect to the mongodb client
+myclient = pymongo.MongoClient("mongodb+srv://choprahetarth:45AJpXuKlK90Xc5s@cluster0.jcnnsrz.mongodb.net/?retryWrites=true&w=majority")
+
+# # # make a new database
+mydb = myclient["twitter_collection"]
+# # # with a new column
+mycol = mydb["comments"]
+
+# # add the clean text column to the mongodb
+mycol.insert_many(comments)
+
