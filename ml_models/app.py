228e9aa69b5f8b33f24be7a86839dab9fbfddc79
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2023-06-02 10:33:01 +0000

    added free api for sentiment

--- ml_models/app.py
+++ ml_models/app.py
@@ -1,20 +1,30 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import requests
 
 
 app = Flask(__name__)
-# CORS(app)  # Enable CORS
 
-# # Initialize sentiment analysis pipeline
-# sentiment_analysis = pipeline("sentiment-analysis")
+
+# using a free API for sentiment analysis 
+url = "https://api.apilayer.com/sentiment/analysis"
+
+
 
 @app.route('/analyze_sentiment', methods=['GET'])
 def analyze_sentiment():
     tweet = request.args.get('text')
     if tweet:
-        # result = sentiment_analysis(tweet)[0]
-        # sentiment = "Positive" if result['label'] == 'POSITIVE' else "Negative"
-        return jsonify(sentiment="j"), 200
+        payload = tweet
+        headers= {
+        "apikey": "T7z2uSqTq9YBRR6dgpQ5crLgTSq93XJl"
+        }
+
+        response = requests.request("POST", url, headers=headers, data = payload)
+        status_code = response.status_code
+        result = response.text
+        sentiment = "positive" if result['label'] == 'positive' else "negative"
+        return jsonify(sentiment=sentiment), 200
     else:
         return jsonify(error="No text provided"), 400
     
