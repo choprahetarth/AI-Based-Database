742d2ba2f9a21a1c4e73ca294a226354243fd71d
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2023-06-02 16:43:23 +0000

    create schema code

--- yml_parser.py
+++ yml_parser.py
@@ -27,7 +27,56 @@ def connect_to_database(yaml_parsed):
     except:
         print("Unexpected error in database")
 
-def create_schema():
-    
+def create_schema(yaml_parsed):
+    # try:
+    conn = psycopg2.connect(
+        host=yaml_parsed['database']['host'],
+        database=yaml_parsed['database']['name'],
+        user=yaml_parsed['database']['user'],
+        password=yaml_parsed['database']['password'],
+        port=yaml_parsed['database']['port'])
+    # initiate the cursor
+    print("Connected to the Database")
+
+    # initiate the cursor
+    cur = conn.cursor()
+    # execute the query
+    # iterate over tables
+    for i,j in enumerate(yaml_parsed['tables'].values()):
+        print(j)
+    # close the connection
+    cur.close()
+    conn.close()
+    # except:
+    #     print("ahahahhahhah")
+    #     cur.execute("""CREATE TABLE IF NOT EXISTS mongodb
+    #     (
+    #         id SERIAL PRIMARY KEY,
+    #         tweet TEXT NOT NULL
+    #     )"""
+    #                 )
+
+    # cur.execute("""CREATE TABLE IF NOT EXISTS sentiment
+    # (
+    #     id INTEGER NOT NULL,
+    #     sentiment BOOLEAN,
+    #     FOREIGN KEY (id) REFERENCES mongodb
+    #     (id) ON DELETE CASCADE
+    # )
+    # """)
+
+    # cur.execute("""  CREATE TABLE closest_topic (
+    #         id INTEGER NOT NULL,
+    #         topic TEXT,
+    #         FOREIGN KEY (id) REFERENCES mongodb(id) ON DELETE CASCADE
+    #     );
+    # """)
+
+    # # close the connection
+    # conn.commit()
+    # cur.close()
+    # conn.close()
+
 yaml_parsed  = read_yaml("config.yaml")
 connect_to_database(yaml_parsed) 
+create_schema(yaml_parsed)
