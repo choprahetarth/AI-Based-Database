0713d92e18064fe8134fa76bb38aab6cf530f5a2
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2023-06-02 16:10:50 +0000

    started adding the YAML Integration

---
+++ yml_parser.py
@@ -0,0 +1,29 @@
+import yaml
+import psycopg2
+
+def read_yaml(path):
+    with open(path, "r") as stream:
+        try:
+            yaml_parsed = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return yaml_parsed
+
+def connect_to_database(yaml_parsed):
+    # connect with the postgres instance
+    try:
+        conn = psycopg2.connect(
+            host=yaml_parsed['database']['host'],
+            database=yaml_parsed['database']['name'],
+            user=yaml_parsed['database']['user'],
+            password=yaml_parsed['database']['password'],
+            host=yaml_parsed[''])
+        # initiate the cursor
+        cur = conn.cursor()
+        # close the connection
+        cur.close()
+        conn.close()
+    except:
+        print("Unexpected error in database")
+
+yaml_parsed  = read_yaml("config.yaml")
