3e24317a75a5c95864ad2a51359a79582dbee710
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2023-06-02 18:20:58 +0000

    yaml to table works

--- yml_parser.py
+++ yml_parser.py
@@ -29,22 +29,61 @@ def connect_to_database(yaml_parsed):
 
 
 def read_structure_of_tables(yaml_parsed):
-    
+    queries = []
+    for table in yaml_parsed["tables"]:
+        if table['is_aidb']==True:
+            prefix="ai_"
+        else:
+            prefix=""
+        # Build the CREATE TABLE query
+        query = f"CREATE TABLE IF NOT EXISTS {prefix}{table['name']} ("
 
+        columns = []
+        for column in table['columns']:
+            col = f"{column['name']} {column['type']}"
 
-# def create_schema(yaml_parsed):
-#     # try:
-#     conn = psycopg2.connect(
-#         host=yaml_parsed['database']['host'],
-#         database=yaml_parsed['database']['name'],
-#         user=yaml_parsed['database']['user'],
-#         password=yaml_parsed['database']['password'],
-#         port=yaml_parsed['database']['port'])
-#     # initiate the cursor
-#     print("Connected to the Database")
-#     # initiate the cursor
-#     cur = conn.cursor()
+            if column.get('not_null', False):
+                col += " NOT NULL"
+
+            if column.get('primary_key', False):
+                col += " PRIMARY KEY"
+
+            if 'foreign_key' in column:
+                reference_table = column['foreign_key']['reference_table']
+                reference_column = column['foreign_key']['reference_column']
+                col += f", FOREIGN KEY ({reference_column}) REFERENCES {reference_table}"
+
+                if column['foreign_key'].get('on_delete_cascade', False):
+                    col += " ON DELETE CASCADE"
+            columns.append(col)
+
+        query += ", ".join(columns)
+        query += ");"
+        queries.append(query)
+    return queries
+
+def execute_queries(queries, yaml_parsed):
+    try:
+        conn = psycopg2.connect(
+            host=yaml_parsed['database']['host'],
+            database=yaml_parsed['database']['name'],
+            user=yaml_parsed['database']['user'],
+            password=yaml_parsed['database']['password'],
+            port=yaml_parsed['database']['port'])
+        # initiate the cursor
+        print("Executed the queries")
+        cur = conn.cursor()
+        for i in queries:
+            cur.execute(i)
+        # close the connection
+        conn.commit()
+        cur.close()
+        conn.close()
+    except:
+        print("Could not execute the queries")
+
 
 yaml_parsed  = read_yaml("config.yaml")
-# connect_to_database(yaml_parsed) 
-# create_schema(yaml_parsed)
+queries = read_structure_of_tables(yaml_parsed)
+execute_queries(queries,yaml_parsed)
+
