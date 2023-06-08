668fa12b413dfecd955b96ced80303bb452fa557
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2023-06-08 19:33:51 +0000

    made the API free for testing on prod, dummy data

--- ml_models/app.py
+++ ml_models/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import requests
+import random
 import json
 
 
@@ -10,17 +11,21 @@ def analyze_sentiment():
     tweet = request.args.get('text')
     if tweet:
         # using a free API for sentiment analysis
-        url = "https://api.apilayer.com/sentiment/analysis"
-        payload = tweet
-        headers= {
-        "apikey": "T7z2uSqTq9YBRR6dgpQ5crLgTSq93XJl"
-        }
+        ##### UNCOMMENT IN PROD ########
+        # url = "https://api.apilayer.com/sentiment/analysis"
+        # payload = tweet
+        # headers= {
+        # "apikey": "T7z2uSqTq9YBRR6dgpQ5crLgTSq93XJl"
+        # }
 
-        response = requests.request("POST", url, headers=headers, data = payload)
-        status_code = response.status_code
-        result = response.text
-        result = json.loads(result)
+        # response = requests.request("POST", url, headers=headers, data = payload)
+        # status_code = response.status_code
+        # result = response.text
+        # result = json.loads(result)
         # sentiment = "positive" if result['label'] == 'positive' else "negative"
+        ################
+        result = {'sentiment':random.randint(0,4)}
+        status_code = 200
         return jsonify(sentiment=result['sentiment']), status_code
     else:
         return jsonify(error="No text provided"), 400
@@ -30,15 +35,18 @@ def analyze_sentiment():
 def provide_ner():
     tweet = request.args.get('text')
     if tweet:
-        url = "https://api.apilayer.com/nlp/named_entity?lang=eng"
-        payload = tweet
-        headers= {
-        "apikey": "T7z2uSqTq9YBRR6dgpQ5crLgTSq93XJl"
-        }
-        response = requests.request("POST", url, headers=headers, data = payload)
-        status_code = response.status_code
-        result = response.text
-        result = json.loads(result)
+        ##### UNCOMMENT IN PROD ########
+        # url = "https://api.apilayer.com/nlp/named_entity?lang=eng"
+        # payload = tweet
+        # headers= {
+        # "apikey": "T7z2uSqTq9YBRR6dgpQ5crLgTSq93XJl"
+        # }
+        # response = requests.request("POST", url, headers=headers, data = payload)
+        # status_code = response.status_code
+        # result = response.text
+        # result = json.loads(result)
+        result = {'result':str(random.randint(5,1000))}
+        status_code = 200
         return jsonify(ner=result['result']), status_code
     else:
         return jsonify(error="No text provided"), 400
